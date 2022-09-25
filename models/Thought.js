188d383226eda4b882e16b9b27ac52e5cb39cb25
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-25 11:04:00 -0500

    setup reactions schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema(
@@ -20,16 +20,43 @@ const ThoughtSchema = new Schema(
             required: true
         },
         reactions: [
-            reactionSchema
+            ReactionSchema
         ]
     },
     {
         toJSON: {
             virtuals: true,
             getters: true,
-            timestamps: true
         },
         id: false
+    }
+);
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
     }
 );
 // virtual that retrieves length of the thoughts reactions array 
